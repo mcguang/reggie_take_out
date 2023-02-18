c3f0e228a7a81c2bcb6e4a085d1943bc5d2f7f36
mcguang <meichengguang@163.com>
2023-02-19 00:53:12 +0800

    pre use redis to cache setmeal

--- src/main/java/com/itheima/reggie/controller/SetmealController.java
+++ src/main/java/com/itheima/reggie/controller/SetmealController.java
@@ -13,6 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -42,6 +44,7 @@ public class SetmealController {
      * @param setmealDto
      * @return
      */
+    @CacheEvict
     @PostMapping
     public R<String> save(@RequestBody SetmealDto setmealDto){
         log.info("套餐信息：{}",setmealDto);
@@ -117,6 +120,7 @@ public class SetmealController {
      * @param ids
      * @return
      */
+    @CacheEvict
     @DeleteMapping
     public R<String> delete(@RequestParam List<Long> ids){
         log.info("ids:{}",ids);
@@ -145,6 +149,7 @@ public class SetmealController {
      * @param setmeal
      * @return
      */
+    @Cacheable(value = "setmealCache",key = "#setmeal.categoryId + '_' + #setmeal.status")
     @GetMapping("/list")
     public R<List<Setmeal>> list(Setmeal setmeal){
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
